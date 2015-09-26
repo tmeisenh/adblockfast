20bb68c670ea75a63c007eb96be6540a5c0171eb
Travis 'All-Father' Meisenheimer <tmeisenh@gmail.com>
2015-09-26 15:35:38 -0500

    Cleaned up

--- ios/blocker/ActionRequestHandler.m
+++ ios/blocker/ActionRequestHandler.m
@@ -22,37 +22,41 @@
 
 @implementation ActionRequestHandler
 
-- (NSUserDefaults *)preferences
-{
+- (NSUserDefaults *)preferences {
     if (!_preferences) {
         _preferences = [[NSUserDefaults alloc] initWithSuiteName:APP_GROUP_ID];
-        [_preferences registerDefaults:[NSDictionary
-                                           dictionaryWithContentsOfFile:
-                                               [[NSBundle mainBundle]
-                                                   pathForResource:DEFAULT_PREFERENCES_FILENAME
-                                                            ofType:PREFERENCES_FILE_EXTENSION]]];
+        NSURL *preferencesFileURL = [[NSBundle mainBundle] URLForResource:DEFAULT_PREFERENCES_FILENAME
+                                                            withExtension:PREFERENCES_FILE_EXTENSION];
+        
+        [_preferences registerDefaults:[NSDictionary dictionaryWithContentsOfURL:preferencesFileURL]];
     }
-
+    
     return _preferences;
 }
 
-- (void)beginRequestWithExtensionContext:(NSExtensionContext *)context
-{
+- (void)beginRequestWithExtensionContext:(NSExtensionContext *)context {
     NSUserDefaults *preferences = self.preferences;
-    if (![preferences boolForKey:BLOCKER_PERMISSION_KEY])
+    if (![preferences boolForKey:BLOCKER_PERMISSION_KEY]) {
         [preferences setBool:YES forKey:BLOCKER_PERMISSION_KEY];
-    NSItemProvider *attachment =
-        [[NSItemProvider alloc]
-            initWithContentsOfURL:[[NSBundle mainBundle]
-                                      URLForResource:
-                                          [preferences boolForKey:BLOCKING_STATUS_KEY] ?
-                                              BLOCKER_RULESET_FILENAME : NOOP_RULESET_FILENAME
-                                       withExtension:RULESET_FILE_EXTENSION]];
+    }
+    
+  
+    NSURL *blockerListURL;
+    if ([preferences boolForKey:BLOCKING_STATUS_KEY]) {
+        blockerListURL = [[NSBundle mainBundle] URLForResource:BLOCKER_RULESET_FILENAME withExtension:RULESET_FILE_EXTENSION];
+    } else {
+        blockerListURL = [[NSBundle mainBundle] URLForResource:NOOP_RULESET_FILENAME withExtension:RULESET_FILE_EXTENSION];
+    }
+    
+    NSItemProvider *attachment = [[NSItemProvider alloc] initWithContentsOfURL:blockerListURL];
+    
     NSExtensionItem *item = [[NSExtensionItem alloc] init];
     item.attachments = @[attachment];
-    [context completeRequestReturningItems:@[item] completionHandler:nil];
+    
     NSInteger rulesetCompileCount = [preferences integerForKey:RULESET_COMPILE_COUNT_KEY];
     [preferences setInteger:++rulesetCompileCount forKey:RULESET_COMPILE_COUNT_KEY];
+    
+    [context completeRequestReturningItems:@[item] completionHandler:nil];
 }
 
 @end
